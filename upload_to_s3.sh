2bbc37d899ca3370218ab504391a68943a334350
vipul-dake-psl <96866150+vipul-dake-psl@users.noreply.github.com>
2022-01-12 17:18:13 +0530

    Create upload_to_s3.sh

---
+++ upload_to_s3.sh
@@ -0,0 +1,44 @@
+#!/bin/sh 
+pip install --quiet --no-cache-dir awscli
+set -e
+
+#!/bin/sh 
+
+set -e
+
+echo "Echo from upload_to_s3.sh file"
+
+# if [ -z "$S3_BUCKET" ]; then
+#   echo "S3_BUCKET is not set. Quitting."
+#   exit 1
+# fi
+# if [ -z "$AWS_ACCESS_KEY_ID" ]; then
+#   echo "AWS_ACCESS_KEY_ID is not set. Quitting."
+#   exit 1
+# fi
+# if [ -z "$AWS_SECRET_ACCESS_KEY" ]; then
+#   echo "AWS_SECRET_ACCESS_KEY is not set. Quitting."
+#   exit 1
+# fi
+
+# if [-z "$FILE"]; then
+#   echo "FILE is not set. Quitting"
+#   exit 1
+# fi
+
+# if [ -z "$AWS_REGION"]; then
+#   AWS_REGION="us-east-1"
+# fi
+
+# mkdir -p ~/.aws
+
+# touch ~/.aws/credentials
+
+# echo "[default]
+#aws_access_key_id = ${AWS_ACCESS_KEY_ID}
+#aws_secret_access_key = ${AWS_SECRET_ACCESS_KEY}" > ~/.aws/credentials
+
+#aws s3 cp ${FILE} s3://${S3_BUCKET}/${S3_KEY} \
+#  --region ${AWS_REGION} $*
+
+#rm -rf ~/.aws
